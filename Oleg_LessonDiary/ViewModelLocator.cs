234a12150cc3da4c725d82fa2304b37dcaa6d696
Lazurigost <lazurigost@gmail.com>
2023-03-03 09:57:24 +0500

    bruh

--- Oleg_LessonDiary/ViewModelLocator.cs
+++ Oleg_LessonDiary/ViewModelLocator.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Oleg_LessonDiary
+﻿namespace Oleg_LessonDiary
 {
-    class ViewModelLocator
+    public internal class ViewModelLocator
     {
     }
 }
