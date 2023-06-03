a21aeeae9df87f95210661dfa57ef3fae991b718
Lazurigost <lazurigost@gmail.com>
2023-06-03 18:49:48 +0500

    Да

--- Oleg_LessonDiary/ViewModels/TeacherUserControlViewModel.cs
+++ Oleg_LessonDiary/ViewModels/TeacherUserControlViewModel.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Oleg_LessonDiary.ViewModels
 {
-    internal class TeacherUserControlViewModel
+    public partial class TeacherUserControlViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private string? teacher_fullname;
+
+        private readonly PageService _pageService;
+
+        public TeacherUserControlViewModel(PageService pageService)
+        {
+            _pageService = pageService;
+            if (CurrentUser.userSaved != null) 
+            {
+                Teacher_fullname = $"{CurrentUser.userSaved.UserSurname} {CurrentUser.userSaved.UserName} {CurrentUser.userSaved.UserPatronymics}";
+            }
+        }\\
+        private void LogOut()
+        {
+            CurrentUser.userSaved = null;
+            _pageService.ChangePage(new SignInPage());
+        }
     }
 }
