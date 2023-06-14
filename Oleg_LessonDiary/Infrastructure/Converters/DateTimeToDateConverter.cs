032dab51b8ba95ab6743016cb6b72eedc866753b
Lazurigost <lazurigost@gmail.com>
2023-06-14 23:11:23 +0500

    Aboba

--- Oleg_LessonDiary/Infrastructure/Converters/DateTimeToDateConverter.cs
+++ Oleg_LessonDiary/Infrastructure/Converters/DateTimeToDateConverter.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 
 namespace Oleg_LessonDiary.Infrastructure.Converters
 {
-    internal class DateTimeToDateConverter
+    public class DateTimeToDateConverter : IValueConverter
     {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((DateTime)value).ToString("dd.MM.yyyy");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
     }
 }
